9918cac5d67919f3db5650031e8d8642f815bac9
rindav <rinat.davlikamov@sitronics-kt.ru>
2021-12-06 15:37:38 +0400

    Add user data

--- src/modules/testModule/components/UserData.vue
+++ src/modules/testModule/components/UserData.vue
@@ -5,8 +5,26 @@
       <v-text-field
         v-model="user.name"
         label="Name"
+        dense
+        outlined
+      />
+      <v-text-field
+        v-model="user.rocket"
+        label="Rocket"
+        dense
+        outlined
+      />
+      <v-text-field
+        v-model="user.twitter"
+        label="Twitter"
+        dense
+        outlined
       />
     </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="primary" @click="save">Save</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -37,15 +55,17 @@ export default class UserData extends Vue {
         id: this.id,
       }
     },
-    update: ({ users }) => {
-      return users[0]
-    },
+    update: ({ users }) => users[0],
   })
   private readonly user: User = {
     id: '',
     name: '',
     rocket: '',
     twitter: '',
+  }
+
+  private save() {
+    // TODO: Save data
   }
 }
 </script>
