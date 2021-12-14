ed62476f2bf03b5c8df81546df9ea0afb172a432
rindav <rinat.davlikamov@sitronics-kt.ru>
2021-12-14 17:28:46 +0400

    Remove about

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,17 +16,6 @@ const routes: Array<RouteConfig> = [
       layout: () => import('@/layouts/BaseLayout.vue'),
     },
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-    meta: {
-      layout: () => import('@/layouts/AboutLayout.vue'),
-    },
-  },
   ...connectedRoutes,
 ]
 
